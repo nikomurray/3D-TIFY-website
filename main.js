372812ea2dc75ce072e919ffc2c8e5e0b7cd0aa5
Nicolas <npereyramurray@gmail.com>
2024-03-11 19:25:27 -0300

    add responsive bars nav

---
+++ main.js
@@ -0,0 +1,6 @@
+let dropBtn = document.getElementById("dropBtn")
+let dropContainer = document.getElementById("dropContainer")
+
+dropBtn.addEventListener("click" , () =>{
+    dropContainer.classList.toggle("show");
+})
